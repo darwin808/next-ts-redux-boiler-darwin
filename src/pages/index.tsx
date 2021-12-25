6b17c814c43b669120eedb704c976438be6a14dc
darwin <darwin.apolinario@serino.com>
2021-12-25 12:14:38 +0800

    feat(main proj): initial
    intial realease

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next"
 
 const Home: NextPage = () => {
-  return <div className="bg-red-200 h-screen">darwin123i123</div>
+  return <div className="bg-red-200 h-screen">darwin123i112323</div>
 }
 
 export default Home
