8dd276e409146bc835727ab8fec7accb3c8f5679
darwin <darwin.apolinario@serino.com>
2022-02-06 16:31:31 +0800

    :lipstick: tailwind update
    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next"
 
 const Home: NextPage = () => {
-  return <div className="bg-red-200 h-screen">estong</div>
+  return <div className="bg-red-200 h-screen">test</div>
 }
 
 export default Home
