472aa9777c5d08a0b2430533297b880737703aa6
darwin <darwin.apolinario@serino.com>
2022-03-13 14:46:05 +0800

    :art: (index.tsx): cleanup clg

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,9 @@
 import "../../styles/globals.css"
 import type { AppProps } from "next/app"
 import { Provider } from "react-redux"
-import { store } from "../redux/store"
-// import { PersistGate } from "reduxjs-toolkit-persist/integration/react"
-// import { PersistGate } from "redux-persist/integration/react"
-// import { PersistGate } from "redux-persist/integration/react"
+import { store } from "redux/store"
 import { persistStore } from "redux-persist"
 import { PersistGate } from "redux-persist/integration/react"
-// import { PersistGate } from "reduxjs-toolkit-persist/integration/react"
 const persistor = persistStore(store)
 
 function MyApp({ Component, pageProps }: AppProps) {
